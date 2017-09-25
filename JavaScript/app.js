f6a5add3ecd34c14d2313537fecf38e9d28598a6
Aplex <alexander.dahlberg@student.kyh.se>
2017-09-25 13:57:34 +0200

    changes
    makin in twerk

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,10 +1,10 @@
-const url = 'http://localhost:8080';
+const url = 'http://localhost:3000';
 
 let userName = prompt("Whats your name?");
 
 
 document.getElementById('submitMessage').onclick = function(){
-	let	tempMessage = document.getElementById("message").value;	
+	let	tempMessage = document.getElementById("message").value;
 	alert(newMessage);
 	let newMessage = {userName: userName, message: tempMessage
 	};
