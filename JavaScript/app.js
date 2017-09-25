14e142dddcdec1ba05fe9ef77c31f51273b72934
liolios <simon.liolios@hyperisland.se>
2017-09-25 13:57:32 +0200

    change port, fix newMessage bug

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,14 +1,14 @@
-const url = 'http://localhost:8080';
+const url = 'http://localhost:3000';
 
 let userName = prompt("Whats your name?");
 
 
 document.getElementById('submitMessage').onclick = function(){
 	let	tempMessage = document.getElementById("message").value;	
-	alert(newMessage);
+	alert(tempMessage);
 	let newMessage = {userName: userName, message: tempMessage
 	};
 	const request = new XMLHttpRequest();
 	request.open('POST', url);
-	request.send(newMessage);
+	request.send(JSON.stringify(newMessage));
 }
