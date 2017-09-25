2510799be2b5d35232dc84f5c64edda1da210da6
Aplex <alexander.dahlberg@student.kyh.se>
2017-09-25 12:59:12 +0200

    server
    bugg slove. console log server start

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const http = require('http');
 const request = require('request');
 const server_port = 3000;
-let messages[];
+let messages = [];
 
 const server = http.createServer(requestHandler);
-server.listen(server_port);
+server.listen(server_port, function(){
+  console.log('Now listening on port 3000');
+});
 
 function requestHandler(request, response){
   response.setHeader('Access-Control-Allow-Origin', '*');
