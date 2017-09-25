0bd6b1d3ba74011fa18898ccfb70d6878a9055bb
Aplex <alexander.dahlberg@student.kyh.se>
2017-09-25 11:42:45 +0200

    I fuck up
    Kappa123

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const http = require(http);
-const request = require(request);
+const http = require('http');
+const request = require('request');
 const server_port = 3000;
 
 const server = http.createServer(requestHandler);
@@ -8,7 +8,7 @@ server.listen(server_port);
 function requestHandler(request, response){
   response.setHeader('Access-Control-Allow-Origin', '*');
   if(request.method === 'GET'){
-      
+
   }else if(request.method === 'POST'){
 
   }
