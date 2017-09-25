c663054544e812738cd1767080e0e3a02c65ac9e
liolios <simon.liolios@hyperisland.se>
2017-09-25 14:21:26 +0200

    add timestamp

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -4,9 +4,14 @@ let userName = prompt("Whats your name?");
 
 
 document.getElementById('submitMessage').onclick = function(){
-	let	tempMessage = document.getElementById("message").value;	
+	let	tempMessage = document.getElementById("message").value;
+	let d = new Date();
+	let time = d.getHours() + ":" + d.getMinutes();	
 	alert(tempMessage);
-	let newMessage = {userName: userName, message: tempMessage
+	let newMessage = {
+		userName: userName, 
+		message: tempMessage,
+		time: time
 	};
 	const request = new XMLHttpRequest();
 	request.open('POST', url);
